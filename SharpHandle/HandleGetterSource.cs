d54c327a440c72a58a706016647e7ec4d3c166a4
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2021-02-24 13:22:08 -0800

    Add project files.

---
+++ SharpHandle/HandleGetterSource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SharpHandle
+{
+	public class HandleGetter
+	{
+		#region PInvoke
+		[DllImport("user32")]
+		private static extern IntPtr FindWindow(string sClassName, string sWindowName);
+
+		[DllImport("user32")]
+		private static extern IntPtr GetForegroundWindow();
+		#endregion
+		#region Functions
+		public static IntPtr GetWindowHandle(string WindowTitle) => FindWindow(null, WindowTitle);
+		public static IntPtr GetFocusedWindowHandle() => GetForegroundWindow();
+		#endregion
+	}
+}
